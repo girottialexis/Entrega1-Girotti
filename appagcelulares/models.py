ba026dc5b4cd9eac0ab9ca351c60165b5f093808
Alexis Girotti <girottialexis@gmail.com>
2022-03-27 16:59:03 -0300

    proyecto inicial entrega

---
+++ appagcelulares/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    dni = models.IntegerField(primary_key=True)
+
+class Equipo(models.Model):
+    modelo_equipo = models.CharField(max_length=40)
+    numero_telefono = models.IntegerField(primary_key=True)
+    
+class Fichatecnica(models.Model):
+    numero_ficha = models.IntegerField(primary_key=True)
+    fecha_recibido = models.DateField()
+    problema_tecnico = models.CharField(max_length=300)
+    reparado = models.BooleanField()
+    fecha_entregado = models.DateField()
